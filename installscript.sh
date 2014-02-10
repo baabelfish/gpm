dd682d832421ca43a1bdf8352291e3f34d336e97
Juho Teperi <juho.teperi@iki.fi>
2014-02-10 20:43:52 +0200

    Start refactoring

--- installscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-[[ ! -e ~/.local/tpm/tpm ]] && ./init
-cp -f tpm ~/.local/tpm/tpm/tpm
-cp -f git.sh ~/.local/tpm/tpm/git.sh
-cp -f helpers.sh ~/.local/tpm/tpm/helpers.sh
-cp -f json.sh ~/.local/tpm/tpm/json.sh
-cp -f source ~/.local/tpm/tpm/source
